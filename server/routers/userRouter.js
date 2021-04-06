87e7e29e5ee1856b41e92d620509925d092b8421
Chris Lee <christopher.lee222@yahoo.com>
2021-04-05 23:02:44 -0500

    'utils_generateToken-dotenv'

--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -1,14 +1,39 @@
-import express from 'express';
-import expressAsyncHandler from 'express-async-handler';
-import User from '../models/userModel.js';
-import data from '../data.js';
+import express from "express";
+import expressAsyncHandler from "express-async-handler";
+import bcrypt from "bcryptjs";
+import User from "../models/userModel.js";
+import data from "../data.js";
+import { generateToken } from "../utils.js";
 
 const userRouter = express.Router();
 
-userRouter.get('/seed', expressAsyncHandler(async(request, response) => {
+userRouter.get(
+  "/seed",
+  expressAsyncHandler(async (request, response) => {
     // await User.remove({});
     const createdUsers = await User.insertMany(data.users);
     response.send({ createdUsers });
-}));
+  })
+);
+
+userRouter.post(
+  "/signin",
+  expressAsyncHandler(async (request, response) => {
+    const user = await User.findOne({ email: request.body.email });
+    if (user) {
+      if (bcrypt.compareSync(request.body.password, user.password)) {
+        response.send({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user),
+        });
+        return;
+      }
+    }
+    response.status(401).send({ message: "Invalid email or password" });
+  })
+);
 
 export default userRouter;
--- server/server.js
+++ server/server.js
@@ -1,27 +1,33 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import productRouter from './routers/productRouter.js';
-import userRouter from './routers/userRouter.js';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import productRouter from "./routers/productRouter.js";
+import userRouter from "./routers/userRouter.js";
+
+dotenv.config();
 
 const app = express();
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/e-commerce', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/e-commerce", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
 });
 
 const port = process.env.PORT || 5000;
 
-app.use('/api/users', userRouter);
-app.use('/api/products', productRouter);
-app.get('/', (request, response) => {
-    response.send(`Server is ready`)
+app.use("/api/users", userRouter);
+app.use("/api/products", productRouter);
+app.get("/", (request, response) => {
+  response.send(`Server is ready`);
 });
 
 app.use((err, request, response, next) => {
-    response.status(500).send({message: err.message});
+  response.status(500).send({ message: err.message });
 });
 
 app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}.  BOOOM!!!`)
+  console.log(`Server running on http://localhost:${port}.  BOOOM!!!`);
 });
